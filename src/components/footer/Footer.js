a0101d654841fe354fa3d56921c1ee2c4cdda57a
Esra <108522212+esraocak@users.noreply.github.com>
2023-04-07 01:45:16 +0300

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
         rel="noopener noreferrer"
         style={{ textDecoration: "none" }}
       >
-        <code className="brand">{"<Clarusway/> "}</code>
+        <code className="brand">{"Web "}</code>
       </a>
       <img
         src={designSvg}
